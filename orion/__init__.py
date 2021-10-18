ea26db8be83cbeb73b74e0cf9a2bf6438fec958f
= <=>
2021-10-18 08:41:04 +0700

    noop

--- orion/__init__.py
+++ orion/__init__.py
@@ -5,3 +5,5 @@ import sys, os;
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)));
 sys.tracebacklimit = 0
+
+from vector import *;
---
+++ orion/vector/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import vector.concept;
+import vector.determine;
+import vector.minor;
+import vector.invers;
